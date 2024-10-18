0ca926534175bd31093d57d7dd973a7d7a86813b
isro24 <alamat_email_anda@example.com>
2024-10-18 22:54:39 +0700

    Menambahkan gambar diri sendiri

--- app/src/main/java/com/example/praktikumkedua/MainActivity.kt
+++ app/src/main/java/com/example/praktikumkedua/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -90,6 +92,11 @@ fun BasicCompose(isro: Modifier = Modifier){
         Text(
             text = "20220140047",
             fontSize = 30.sp,
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.gambar),
+            contentDescription = null,
         )
 
     }
