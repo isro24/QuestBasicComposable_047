86590998a5b7e26ad5d5112bbc48e12c6753cb69
isro24 <alamat_email_anda@example.com>
2024-10-18 22:44:48 +0700

    Menambahkan space

--- app/src/main/java/com/example/praktikumkedua/MainActivity.kt
+++ app/src/main/java/com/example/praktikumkedua/MainActivity.kt
@@ -6,15 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikumkedua.ui.theme.PraktikumKeduaTheme
 
@@ -52,6 +58,14 @@ fun BasicCompose(isro: Modifier = Modifier){
 
         Text(
             text = "Ini adalah halaman Login",
+            style = TextStyle(
+                fontSize = 15.sp,
+                fontStyle = FontStyle.Normal,
+                color = Color.Black
+            )
         )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
     }
 }
