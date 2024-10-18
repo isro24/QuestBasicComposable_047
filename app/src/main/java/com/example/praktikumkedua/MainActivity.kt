f4c5dc203e87797da11ab6ff61e556eab4f621df
isro24 <alamat_email_anda@example.com>
2024-10-18 22:48:06 +0700

    Mengatur size Gambar umy

--- app/src/main/java/com/example/praktikumkedua/MainActivity.kt
+++ app/src/main/java/com/example/praktikumkedua/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -72,6 +73,7 @@ fun BasicCompose(isro: Modifier = Modifier){
         Image(
             painter = painterResource(id = R.drawable.logoumy),
             contentDescription = null,
+            modifier = isro.size(250.dp)
         )
 
     }
