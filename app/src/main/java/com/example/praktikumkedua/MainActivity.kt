12e47accb76a058ef381d9e2034b773cf8be975e
isro24 <alamat_email_anda@example.com>
2024-10-18 22:41:17 +0700

    Mengatur size dan berat font

--- app/src/main/java/com/example/praktikumkedua/MainActivity.kt
+++ app/src/main/java/com/example/praktikumkedua/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.praktikumkedua.ui.theme.PraktikumKeduaTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,6 +46,8 @@ fun BasicCompose(isro: Modifier = Modifier){
         Text(
             text = "Login",
             modifier = isro,
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold
         )
     }
 }
